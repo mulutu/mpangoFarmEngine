68b7c8366b2090633e05f6be28e746c29237be40
Mulutu <mulutujackson"gmail.com>
2019-09-05 10:36:04 +0300

    Created PrivilegeDao.java
    Created PrivilegeDao.java

--- src/main/java/com/mpangoEngine/core/dao/PrivilegeDao.java
+++ src/main/java/com/mpangoEngine/core/dao/PrivilegeDao.java
@@ -1,7 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mpangoEngine.core.dao;
 
 import com.mpangoEngine.core.model.Privilege;
 
+/**
+ *
+ * @author jamulutu
+ */
 public interface PrivilegeDao {
 	Privilege findPrivilegeByName(String name);
 
