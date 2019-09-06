8b246c11d7b3b4f714058f82fcafeb27c059c28c
Mulutu <mulutujackson"gmail.com>
2019-09-06 11:07:58 +0300

    asdas
    askdjfsd f

--- src/main/java/com/mpangoEngine/core/dao/impl/TransactionDaoImpl.java
+++ src/main/java/com/mpangoEngine/core/dao/impl/TransactionDaoImpl.java
@@ -19,189 +19,237 @@ import org.springframework.stereotype.Component;
 
 import com.mpangoEngine.core.dao.TransactionDao;
 import com.mpangoEngine.core.model.Transaction;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 
 @Component
 @Transactional
 public class TransactionDaoImpl extends JdbcDaoSupport implements TransactionDao {
 
-	public static final Logger logger = LoggerFactory.getLogger(TransactionDaoImpl.class);
+    public static final Logger logger = LoggerFactory.getLogger(TransactionDaoImpl.class);
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public TransactionDaoImpl(DataSource dataSource) {
-		super();
-		setDataSource(dataSource);
-	}
+    @Autowired
+    public TransactionDaoImpl(DataSource dataSource) {
+        super();
+        setDataSource(dataSource);
+    }
 
-	@Override
-	public Boolean transactionExistsById(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Boolean transactionExistsById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Transaction findTransactionById(int id) {
-		String Query = "SELECT e.id, e.account_id, transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.payment_method_id, e.user_id, "
-				+ "p.project_name, f.farm_name, coa.account_name "
-				+ "FROM transactions e, users u, projects p, farm f, accounts coa "
-				+ "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id and e.id = "
-				+ id;
+    @Override
+    public Transaction findTransactionById(int id) {
+        String Query = "SELECT e.id, e.account_id, transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.payment_method_id, e.user_id, "
+                + "p.project_name, f.farm_name, coa.account_name "
+                + "FROM transactions e, users u, projects p, farm f, accounts coa "
+                + "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id and e.id = "
+                + id;
 
-		logger.debug("TransactionDaoImpl->findTransactionById() >>> Query {} ", Query);
+        logger.debug("TransactionDaoImpl->findTransactionById() >>> Query {} ", Query);
 
-		List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);
 
-		Transaction transaction = new Transaction();
+        Transaction transaction = new Transaction();
 
-		for (Map<String, Object> row : rows) {
-			transaction.setId((int) row.get("id"));
-			transaction.setAccountId((int) row.get("account_id"));
-			transaction.setAmount((BigDecimal) row.get("amount"));
-			transaction.setProjectId((int) row.get("project_id"));
-			transaction.setTransactionDate((Date) row.get("transaction_date"));
-			transaction.setDescription((String) row.get("description"));
-			transaction.setUserId((int) row.get("user_id"));
-			transaction.setProjectName((String) row.get("project_name"));
-			transaction.setFarmName((String) row.get("farm_name"));
-			transaction.setAccountName((String) row.get("account_name"));
+        for (Map<String, Object> row : rows) {
+            transaction.setId((int) row.get("id"));
+            transaction.setAccountId((int) row.get("account_id"));
+            transaction.setAmount((BigDecimal) row.get("amount"));
+            transaction.setProjectId((int) row.get("project_id"));
+            transaction.setTransactionDate((Date) row.get("transaction_date"));
+            transaction.setDescription((String) row.get("description"));
+            transaction.setUserId((int) row.get("user_id"));
+            transaction.setProjectName((String) row.get("project_name"));
+            transaction.setFarmName((String) row.get("farm_name"));
+            transaction.setAccountName((String) row.get("account_name"));
 
-			transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+            transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
 
-			int trxTypeId = (int) row.get("transaction_type_id");
-			if (trxTypeId == 0) {
-				transaction.setTransactionType("INCOME");
-			} else if (trxTypeId == 1) {
-				transaction.setTransactionType("EXPENSE");
-			}
-		}
-		return transaction;
-	}
+            int trxTypeId = (int) row.get("transaction_type_id");
+            if (trxTypeId == 0) {
+                transaction.setTransactionType("INCOME");
+            } else if (trxTypeId == 1) {
+                transaction.setTransactionType("EXPENSE");
+            }
+        }
+        return transaction;
+    }
 
-	@Override
-	public int saveTransaction(Transaction transaction) {
-		
-		logger.debug("TransactionDaoImpl->saveTransaction() >>> Transaction {} ", transaction);
-		
-		String Query = "INSERT INTO transactions "
-				+ "(`id`, `account_id`, `transaction_type_id`,`amount`, `transaction_date`, `description`, `user_id`, `project_id`) "
-				+ "VALUES (?,?,?,?,?,?,?,?)";
-		
-		logger.debug("TransactionDaoImpl->updateTransaction() >>> Query {} ", Query);		
+    @Override
+    public int saveTransaction(Transaction transaction) {
 
-		Object[] params = { transaction.getId(), transaction.getAccountId(), transaction.getTransactionTypeId(),
-				transaction.getAmount(), transaction.getTransactionDate(), transaction.getDescription(), transaction.getUserId(), transaction.getProjectId() };
-		
-		int[] types = { Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.DECIMAL, Types.DATE, Types.VARCHAR, Types.INTEGER, Types.INTEGER };
+        int response = 1;
 
-		return getJdbcTemplate().update(Query, params, types);
-	}
+        logger.debug("TransactionDaoImpl->saveTransaction() >>> Transaction {} ", transaction);
+        logger.debug("TransactionDaoImpl->saveTransaction() >>> Transaction Selected Projects {} ", transaction.getSelectedProjectIds());
 
-	@Override
-	public int updateTransaction(Transaction transaction) {
-		
-		logger.debug("TransactionDaoImpl->updateTransaction() >>> transaction {} ", transaction);
-		
-		String Query = " UPDATE transactions "
-				+ " SET account_id=?, transaction_type_id=?, amount=?, transaction_date=?, description=?, project_id=? "
-				+ " WHERE id=?";		
+        String Query = "INSERT INTO transactions "
+                + "(`id`, `account_id`, `transaction_type_id`,`amount`, `transaction_date`, `description`, `user_id`, `project_id`) "
+                + "VALUES (?,?,?,?,?,?,?,?)";
 
-		logger.debug("TransactionDaoImpl->updateTransaction() >>> Query {} ", Query);
+        List<Integer> selectedProjectIds = transaction.getSelectedProjectIds();
+        int numberOftransactions = selectedProjectIds.size();
 
-		Object[] params = { transaction.getAccountId(), transaction.getTransactionTypeId(), transaction.getAmount(),
-				transaction.getTransactionDate(), transaction.getDescription(),
-				 transaction.getProjectId(), transaction.getId() };
-		
-		int[] types = { Types.INTEGER, Types.INTEGER, Types.DECIMAL, Types.DATE, Types.VARCHAR, Types.INTEGER, Types.INTEGER };
+        BigDecimal amtFull = transaction.getAmount();
 
-		return getJdbcTemplate().update(Query, params, types);
-	}
+        BigDecimal allocatedAmt = amtFull.divide(new BigDecimal(numberOftransactions));
 
-	@Override
-	public List<Transaction> findAll() {
-		String Query = "SELECT e.id, e.account_id, e.transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.user_id, "
-				+ "p.project_name, f.farm_name, coa.account_name "
-				+ "FROM transactions e, users u, projects p, farm f, accounts coa "
-				+ "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id ";
+        for (Integer projectId : selectedProjectIds) {
+            logger.debug("TransactionDaoImpl->saveTransaction() >>> Transaction Selected Project ID: {} ", projectId);
+            Object[] params = {transaction.getId(), transaction.getAccountId(), transaction.getTransactionTypeId(), allocatedAmt, transaction.getTransactionDate(), transaction.getDescription(), transaction.getUserId(), projectId};
+            int[] types = {Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.DECIMAL, Types.DATE, Types.VARCHAR, Types.INTEGER, Types.INTEGER};
 
-		logger.debug("TransactionDaoImpl->findAll() >>> Query {} ", Query);
+            getJdbcTemplate().update(Query, params, types);
 
-		List<Transaction> transactions = new ArrayList<Transaction>();
+            logger.debug("TransactionDaoImpl->updateTransaction() >>> Query {} ", Query);
+        }
 
-		List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);
+        return response;
+    }
 
-		Transaction transaction = new Transaction();
+    /*public void insertListOfPojos(final List<Integer> projectIdList, String sql) {
+    
+        https://www.tutorialspoint.com/springjdbc/springjdbc_batch_operation.htm
+        http://www.java2s.com/Code/Java/Spring/UseBatchPreparedStatementSetter.htm
 
-		for (Map<String, Object> row : rows) {
-			transaction.setId((int) row.get("id"));
-			transaction.setAccountId((int) row.get("account_id"));
-			transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
-			transaction.setAmount((BigDecimal) row.get("amount"));
-			transaction.setProjectId((int) row.get("project_id"));
-			transaction.setTransactionDate((Date) row.get("transaction_date"));
-			transaction.setDescription((String) row.get("description"));
-			transaction.setUserId((int) row.get("user_id"));
-			transaction.setProjectName((String) row.get("project_name"));
-			transaction.setFarmName((String) row.get("farm_name"));
-			transaction.setAccountName((String) row.get("account_name"));
+        //String sql = "INSERT INTO MY_TABLE (FIELD_1,FIELD_2,FIELD_3) VALUES (?,?,?)";
 
-			transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+        getJdbcTemplate().batchUpdate(sql, new BatchPreparedStatementSetter() {
 
-			int trxTypeId = (int) row.get("transaction_type_id");
-			if (trxTypeId == 0) {
-				transaction.setTransactionType("INCOME");
-			} else if (trxTypeId == 1) {
-				transaction.setTransactionType("EXPENSE");
-			}
+            @Override
+            public void setValues(PreparedStatement ps, int i)
+                    throws SQLException {
 
-			transactions.add(transaction);
-		}
-		return transactions;
-	}
+                Integer projId = projectIdList.get(i);
+                ps.setInt(1, projId);
+                ps.setString(2, projId);
+                ps.setString(3, projId);
 
-	@Override
-	public List<Transaction> findUserTransactions(int userId) {		
-		String Query = "SELECT e.id as transaction_id, e.account_id, e.transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.user_id, "
-				+ "p.project_name, f.id as farm_id, f.farm_name, coa.account_name " 
-						+ " FROM transactions e, users u, projects p, farm f, accounts coa "
-						+ "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id and e.user_id = " + userId; 
+            }
 
-		logger.debug("TransactionDaoImpl->findUserTransactions() >>> Query {} ", Query);
+            @Override
+            public int getBatchSize() {
+                return projectIdList.size();
+            }
 
-		List<Transaction> transactions = new ArrayList<Transaction>();
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
 
-		List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);		
+    }*/
+    @Override
+    public int updateTransaction(Transaction transaction) {
 
-		for (Map<String, Object> row : rows) {
-			
-			Transaction transaction = new Transaction();
-			
-			transaction.setId((int) row.get("transaction_id"));
-			transaction.setAccountId((int) row.get("account_id"));
-			transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
-			transaction.setAmount((BigDecimal) row.get("amount"));
-			transaction.setProjectId((int) row.get("project_id"));
-			transaction.setTransactionDate((Date) row.get("transaction_date"));
-			transaction.setDescription((String) row.get("description"));
-			transaction.setUserId((int) row.get("user_id"));
-			transaction.setProjectName((String) row.get("project_name"));
-			transaction.setFarmId((int) row.get("farm_id"));
-			transaction.setFarmName((String) row.get("farm_name"));
-			transaction.setAccountName((String) row.get("account_name"));
+        logger.debug("TransactionDaoImpl->updateTransaction() >>> transaction {} ", transaction);
 
-			transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+        String Query = " UPDATE transactions "
+                + " SET account_id=?, transaction_type_id=?, amount=?, transaction_date=?, description=?, project_id=? "
+                + " WHERE id=?";
 
-			int trxTypeId = (int) row.get("transaction_type_id");
-			if (trxTypeId == 0) {
-				transaction.setTransactionType("INCOME");
-			} else if (trxTypeId == 1) {
-				transaction.setTransactionType("EXPENSE");
-			}
+        logger.debug("TransactionDaoImpl->updateTransaction() >>> Query {} ", Query);
 
-			transactions.add(transaction);
-		}
-		return transactions;
-	}
+        Object[] params = {transaction.getAccountId(), transaction.getTransactionTypeId(), transaction.getAmount(),
+            transaction.getTransactionDate(), transaction.getDescription(),
+            transaction.getProjectId(), transaction.getId()};
+
+        int[] types = {Types.INTEGER, Types.INTEGER, Types.DECIMAL, Types.DATE, Types.VARCHAR, Types.INTEGER, Types.INTEGER};
+
+        return getJdbcTemplate().update(Query, params, types);
+    }
+
+    @Override
+    public List<Transaction> findAll() {
+        String Query = "SELECT e.id, e.account_id, e.transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.user_id, "
+                + "p.project_name, f.farm_name, coa.account_name "
+                + "FROM transactions e, users u, projects p, farm f, accounts coa "
+                + "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id ";
+
+        logger.debug("TransactionDaoImpl->findAll() >>> Query {} ", Query);
+
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);
+
+        Transaction transaction = new Transaction();
+
+        for (Map<String, Object> row : rows) {
+            transaction.setId((int) row.get("id"));
+            transaction.setAccountId((int) row.get("account_id"));
+            transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+            transaction.setAmount((BigDecimal) row.get("amount"));
+            transaction.setProjectId((int) row.get("project_id"));
+            transaction.setTransactionDate((Date) row.get("transaction_date"));
+            transaction.setDescription((String) row.get("description"));
+            transaction.setUserId((int) row.get("user_id"));
+            transaction.setProjectName((String) row.get("project_name"));
+            transaction.setFarmName((String) row.get("farm_name"));
+            transaction.setAccountName((String) row.get("account_name"));
+
+            transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+
+            int trxTypeId = (int) row.get("transaction_type_id");
+            if (trxTypeId == 0) {
+                transaction.setTransactionType("INCOME");
+            } else if (trxTypeId == 1) {
+                transaction.setTransactionType("EXPENSE");
+            }
+
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> findUserTransactions(int userId) {
+        String Query = "SELECT e.id as transaction_id, e.account_id, e.transaction_type_id, e.amount, e.project_id, e.transaction_date, e.description, e.user_id, "
+                + "p.project_name, f.id as farm_id, f.farm_name, coa.account_name "
+                + " FROM transactions e, users u, projects p, farm f, accounts coa "
+                + "WHERE u.id=e.user_id and p.id = e.project_id and p.farm_id = f.id and coa.id = e.account_id and e.user_id = " + userId;
+
+        logger.debug("TransactionDaoImpl->findUserTransactions() >>> Query {} ", Query);
+
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(Query);
+
+        for (Map<String, Object> row : rows) {
+
+            Transaction transaction = new Transaction();
+
+            transaction.setId((int) row.get("transaction_id"));
+            transaction.setAccountId((int) row.get("account_id"));
+            transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+            transaction.setAmount((BigDecimal) row.get("amount"));
+            transaction.setProjectId((int) row.get("project_id"));
+            transaction.setTransactionDate((Date) row.get("transaction_date"));
+            transaction.setDescription((String) row.get("description"));
+            transaction.setUserId((int) row.get("user_id"));
+            transaction.setProjectName((String) row.get("project_name"));
+            transaction.setFarmId((int) row.get("farm_id"));
+            transaction.setFarmName((String) row.get("farm_name"));
+            transaction.setAccountName((String) row.get("account_name"));
+
+            transaction.setTransactionTypeId((int) row.get("transaction_type_id"));
+
+            int trxTypeId = (int) row.get("transaction_type_id");
+            if (trxTypeId == 0) {
+                transaction.setTransactionType("INCOME");
+            } else if (trxTypeId == 1) {
+                transaction.setTransactionType("EXPENSE");
+            }
+
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
 
 }
--- src/main/java/com/mpangoEngine/core/model/Transaction.java
+++ src/main/java/com/mpangoEngine/core/model/Transaction.java
@@ -9,177 +9,191 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.mpangoEngine.core.util.JsonDateDeserializer;
 import com.mpangoEngine.core.util.JsonDateSerializer;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Transaction {
 
-	private int id;
-	private int accountId;
-	private BigDecimal amount;
-	private Date transactionDate;
-	private int transactionTypeId;
-	private String transactionType;
-	private String description;
-	private int projectId;
-	private String projectName;
-	private int userId;	
-	private int farmId;
-	private String farmName;
-	private String accountName;
-	
+    private int id;
+    private int accountId;
+    private BigDecimal amount;
+    private Date transactionDate;
+    private int transactionTypeId;
+    private String transactionType;
+    private String description;
+    private int projectId;
+    private String projectName;
+    private int userId;
+    private int farmId;
+    private String farmName;
+    private String accountName;
 
-	public Transaction() {
-	}	
-	
-	public Transaction( Date transactionDate, BigDecimal amount, int accountId, int projectId, String description, int userId  ) {
-		this.transactionDate = transactionDate;
-		this.amount = amount;
-		this.accountId =  accountId;
-		this.projectId = projectId;
-		this.description = description;
-		this.userId = userId;	
-	}	
-	
-	public Transaction( Date transactionDate, BigDecimal amount, int accountId, int projectId, String description, int userId, int transactionTypeId ) {
-		this.transactionDate = transactionDate;
-		this.amount = amount;
-		this.accountId =  accountId;
-		this.projectId = projectId;
-		this.description = description;
-		this.userId = userId;
-		this.transactionTypeId = transactionTypeId;		
-	}	
-	
-	public Transaction( int id, Date transactionDate, BigDecimal amount, int accountId, int projectId, String description ) {
-		this.id = id;
-		this.transactionDate = transactionDate;
-		this.amount = amount;
-		this.accountId =  accountId;
-		this.projectId = projectId;
-		this.description = description;	
-	}	
-	
-	
+    private List<Integer> selectedProjectIds = new ArrayList<Integer>();
 
-	public int getFarmId() {
-		return farmId;
-	}
+    public Transaction() {
+    }
 
-	public void setFarmId(int farmId) {
-		this.farmId = farmId;
-	}
+    public Transaction(Date transactionDate, BigDecimal amount, int accountId, int projectId, String description, int userId) {
+        this.transactionDate = transactionDate;
+        this.amount = amount;
+        this.accountId = accountId;
+        this.projectId = projectId;
+        this.description = description;
+        this.userId = userId;
+    }
 
-	public String getFarmName() {
-		return farmName;
-	}
+    public Transaction(Date transactionDate, BigDecimal amount, int accountId, int projectId, String description, int userId, int transactionTypeId) {
+        this.transactionDate = transactionDate;
+        this.amount = amount;
+        this.accountId = accountId;
+        this.projectId = projectId;
+        this.description = description;
+        this.userId = userId;
+        this.transactionTypeId = transactionTypeId;
+    }
 
-	public void setFarmName(String farmName) {
-		this.farmName = farmName;
-	}
+    public Transaction(int id, Date transactionDate, BigDecimal amount, int accountId, int projectId, String description) {
+        this.id = id;
+        this.transactionDate = transactionDate;
+        this.amount = amount;
+        this.accountId = accountId;
+        this.projectId = projectId;
+        this.description = description;
+    }
 
-	public String getAccountName() {
-		return accountName;
-	}
+    /**
+     * @return the selectedProjectIds
+     */
+    public List<Integer> getSelectedProjectIds() {
+        return selectedProjectIds;
+    }
 
-	public void setAccountName(String accountName) {
-		this.accountName = accountName;
-	}
+    /**
+     * @param selectedProjectIds the selectedProjectIds to set
+     */
+    public void setSelectedProjectIds(List<Integer> selectedProjectIds) {
+        this.selectedProjectIds = selectedProjectIds;
+    }
 
-	public String getTransactionType() {
-		return transactionType;
-	}
+    public int getFarmId() {
+        return farmId;
+    }
 
-	public void setTransactionType(String transactionType) {
-		this.transactionType = transactionType;
-	}
+    public void setFarmId(int farmId) {
+        this.farmId = farmId;
+    }
 
-	public String getProjectName() {
-		return projectName;
-	}
+    public String getFarmName() {
+        return farmName;
+    }
 
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
+    public void setFarmName(String farmName) {
+        this.farmName = farmName;
+    }
 
+    public String getAccountName() {
+        return accountName;
+    }
 
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public String getTransactionType() {
+        return transactionType;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
 
-	public int getAccountId() {
-		return accountId;
-	}
+    public String getProjectName() {
+        return projectName;
+    }
 
-	public void setAccountId(int accountId) {
-		this.accountId = accountId;
-	}
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
 
-	public BigDecimal getAmount() {
-		return amount;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	@JsonSerialize(using = JsonDateSerializer.class)
-	public Date getTransactionDate() {
-		return transactionDate;
-	}
+    public int getAccountId() {
+        return accountId;
+    }
 
-	@JsonDeserialize(using = JsonDateDeserializer.class, as = Date.class)
-	public void setTransactionDate(Date transactionDate) {
-		this.transactionDate = transactionDate;
-	}
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
 
-	public int getTransactionTypeId() {
-		return transactionTypeId;
-	}
+    public BigDecimal getAmount() {
+        return amount;
+    }
 
-	public void setTransactionTypeId(int transactionTypeId) {
-		this.transactionTypeId = transactionTypeId;
-	}
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    @JsonSerialize(using = JsonDateSerializer.class)
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    @JsonDeserialize(using = JsonDateDeserializer.class, as = Date.class)
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 
-	public int getProjectId() {
-		return projectId;
-	}
+    public int getTransactionTypeId() {
+        return transactionTypeId;
+    }
 
-	public void setProjectId(int projectId) {
-		this.projectId = projectId;
-	}
+    public void setTransactionTypeId(int transactionTypeId) {
+        this.transactionTypeId = transactionTypeId;
+    }
 
-	public int getUserId() {
-		return userId;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	@Override
-	public String toString() {
-		return "Transaction [" 
-								+ "id=" + id + ", " 
-								+ "accountId=" + accountId + ", " 
-								+ "amount=" + amount + ", "
-								+ "transactionDate=" + transactionDate + ", " 
-								+ "transactionTypeId=" + transactionTypeId + ", "
-								+ "description=" + description + ", "
-								+ "projectId=" + projectId + ", " 
-								+ "userId=" + userId 
-							+ "]";
-	}
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction ["
+                + "id=" + id + ", "
+                + "accountId=" + accountId + ", "
+                + "amount=" + amount + ", "
+                + "transactionDate=" + transactionDate + ", "
+                + "transactionTypeId=" + transactionTypeId + ", "
+                + "description=" + description + ", "
+                + "projectId=" + projectId + ", "
+                + "userId=" + userId
+                + "]";
+    }
+
 }
